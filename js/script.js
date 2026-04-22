e623c163b8ce6186d30d0795dc8de9b39bde5cc3
hyunjaeha <gkguswo1231@gmail.com>
2026-04-22 12:36:37 +0900

    design: home header 및 ui

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-// 온보딩 카드 선택 시
-<script>
+//온보딩 카드 선택 시
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".option-card");
   const nextBtn = document.querySelector(".onboarding-next-btn");
 
   cards.forEach((card) => {
     card.addEventListener("click", () => {
+      // 기존 선택 제거
       cards.forEach((c) => c.classList.remove("is-selected"));
+
+      // 선택
       card.classList.add("is-selected");
 
-      nextBtn.disabled = false; 
+      // 버튼 활성화
+      if (nextBtn) nextBtn.disabled = false;
     });
   });
-</script>
+});
